0ab4d7e3ba36f2281024403f37f0055a69d3c7ac
Mateo_Bc <mateobecettecura12345@gmail.com>
2020-06-25 13:43:45 -0300

    puntos 1 y 2 resueltos

--- Ejercicios Django/Ejercicio_2/tienda/models.py
+++ Ejercicios Django/Ejercicio_2/tienda/models.py
@@ -18,6 +18,7 @@ class Telefono(models.Model):
         return str(self.numero)
 
 
+
 class Cliente(models.Model):
     rut = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=35)
@@ -53,3 +54,13 @@ class Producto(models.Model):
 
     def __str__(self):
         return str(self.id + ' ' + self.nombre)
+
+class Detalle_Venta(models.Model):
+    cantidad = models.IntegerField()
+    #producto
+
+class Venta(models.Model):
+    id = models.AutoField(primary_key=True)
+    fecha = models.DateField()
+    descuento = models.IntegerField()
+    #monto final
