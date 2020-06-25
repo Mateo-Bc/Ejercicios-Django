a999f8b98140dd8b269572fdda44d6d4fa9d3303
Mateo_Bc <mateobecettecura12345@gmail.com>
2020-06-25 13:31:56 -0300

    creo unos cuantos models

--- Ejercicios Django/Ejercicio_2/tienda/models.py
+++ Ejercicios Django/Ejercicio_2/tienda/models.py
@@ -41,9 +41,15 @@ class Categoria(models.Model):
     id = models.AutoField(primary_key=True)
     descricion = models.CharField
 
+    def __str__(self):
+        return str(self.descricion)
+
 class Producto(models.Model):
     id = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=35)
     precio = models.IntegerField()
     stock = models.IntegerField()
     #proveedor
+
+    def __str__(self):
+        return str(self.id + ' ' + self.nombre)
