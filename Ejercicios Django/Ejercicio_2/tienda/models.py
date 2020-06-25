d113db65e7cd493fc52bafd73ce3db691faebab7
Mateo_Bc <mateobecettecura12345@gmail.com>
2020-06-25 13:22:35 -0300

    Directorio de unidad 2 (ejercicios con django)

---
+++ Ejercicios Django/Ejercicio_2/tienda/models.py
@@ -0,0 +1,49 @@
+from django.db import models
+
+# Create your models here.
+
+class Direccion(models.Model):
+    calle = models.CharField(max_length=25)
+    numero = models.IntegerField()
+    ciudad = models.CharField(max_length=25)
+    comuna = models.IntegerField()
+
+    def __str__(self):
+        return str(self.calle + ' ' + self.numero)
+
+class Telefono(models.Model):
+    numero = models.IntegerField()
+
+    def __str__(self):
+        return str(self.numero)
+
+
+class Cliente(models.Model):
+    rut = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=35)
+    #telefono
+    #direccion
+
+    def __str__(self):
+        return str(self.rut + ' | ' + self.nombre)
+
+class Proveedor(models.Model):
+    rut = models.AutoField(primary_key=True) 
+    nombre = models.CharField(max_length=35)
+    web = models.CharField(max_length=150)
+    #telefono
+    #direccion
+
+    def __str__(self):
+        return str(self.rut + ' | ' + self.nombre + '(Prov.)')
+
+class Categoria(models.Model):
+    id = models.AutoField(primary_key=True)
+    descricion = models.CharField
+
+class Producto(models.Model):
+    id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=35)
+    precio = models.IntegerField()
+    stock = models.IntegerField()
+    #proveedor
