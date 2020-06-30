aae358c11969f344fdf5c282be6566e2b93bc7ea
Mateo_Bc <mateobecettecura12345@gmail.com>
2020-06-30 20:06:23 -0300

    punto 5 resuelto

--- Ejercicios Django/Ejercicio_2/tienda/admin.py
+++ Ejercicios Django/Ejercicio_2/tienda/admin.py
@@ -23,13 +23,19 @@ class Producto_Admin(admin.ModelAdmin):
 class Venta_Admin(admin.ModelAdmin):
     pass
 
+
+
+class Proveedor_Admin(admin.ModelAdmin):
+    list_display = ['nombre', 'web']
+    list_filter = ('nombre', 'rut')
+
 # Register your models here.
 
 admin.site.register(Direccion,)
 admin.site.register(Telefono, )
 
 admin.site.register(Cliente, Cliente_Admin)
-admin.site.register(Proveedor, )
+admin.site.register(Proveedor, Proveedor_Admin)
 admin.site.register(Categoria, )
 admin.site.register(Producto, Producto_Admin)
 admin.site.register(Venta, Venta_Admin)
